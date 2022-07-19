e5f6517ee21f85fee7eaf94be2f69e898af6c90f
Douglas Souza <douglas.barbosa@sensedia.com>
2022-07-18 21:46:00 -0300

    Start writing codes

--- rabbitmq.producer/src/main/java/com/course/rabbitmq/producer/Application.java
+++ rabbitmq.producer/src/main/java/com/course/rabbitmq/producer/Application.java
@@ -1,13 +1,26 @@
 package com.course.rabbitmq.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.course.rabbitmq.producer.producer.HelloRabbitProducer;
 
 @SpringBootApplication
-public class Application {
+@EnableScheduling
+public class Application implements CommandLineRunner {
+
+	//@Autowired
+	//private HelloRabbitProducer producer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//producer.sendHello("Douglas Souza" + Math.random());
+	}
 }
